e0e9a0849a88d1707d29954739b9c70f48979f22
Samuel Guillemet <samu.guillemet0@gmail.com>
2022-05-02 13:07:13 +0200

    Create 404 page

--- SiteWebLS/SiteWebLS/settings.py
+++ SiteWebLS/SiteWebLS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s%t8-+d9363-fzr94^aa8&kkw2w%f8*q=!*w^9_izaicbm%1f3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '192.168.0.182',
