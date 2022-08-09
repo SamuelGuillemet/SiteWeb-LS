cf9d45a3def71ff47b4004befc9f917dc264ce1c
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-08-09 19:25:46 +0200

    Add detail and index page for Gallery

--- SiteWebLS/GalleryPage/urls.py
+++ SiteWebLS/GalleryPage/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'GalleryPage'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:id>/', views.detail, name='detail')
 ]
--- SiteWebLS/GalleryPage/views.py
+++ SiteWebLS/GalleryPage/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from .models import Photo
 
 # Create your views here.
 
 
 def home(request):
+    queryset = Photo.objects.all()
+    
     breadcrumbs_list = [
         {'link': reverse('HomePage:home'),
          'text': "Accueil"
@@ -15,6 +18,31 @@ def home(request):
     ]
 
     context = {
+        'photo_list': queryset,
         'breadcrumbs_list': breadcrumbs_list
     }
     return render(request, 'GalleryPage/index.html', context)
+
+def detail(request, id): 
+    photo = get_object_or_404(Photo, pk=id)
+    breadcrumbs_list = [
+        {
+            'link': reverse('HomePage:home'),
+            'text': "Accueil"
+        },
+        {
+            'link': reverse('GalleryPage:home'),
+            'text': "Galerie photos"
+        },
+        {
+            'link': reverse('GalleryPage:detail', args=[photo.pk]),
+            'text': photo
+        }
+    ]
+        
+    context = {
+            'photo': photo,
+            'breadcrumbs_list': breadcrumbs_list
+    }
+        
+    return render(request, 'GalleryPage/detail.html', context)
