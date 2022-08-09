39946f00e607377d5c56b8d4ada4615819bd0919
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-08-09 19:24:50 +0200

    Fix on name storage of file

--- SiteWebLS/PodcastPage/models.py
+++ SiteWebLS/PodcastPage/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 def directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'podcasts/{1}'.format(filename)
+    return 'podcasts/{0}'.format(filename)
 
 
 class Podcast(models.Model):
